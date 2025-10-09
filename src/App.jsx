dc158efb5551d272d7c1bd8b567c1a41bd1d6dec
Youssef Atef <hh3928850@gmail.com>
2025-10-09 22:46:03 +0300

    Return Things to it's Normal Way

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
        
         
          <Routes>
-            <Route path="/E-Commerce" element={<HomePage />} />
+            <Route path="/" element={<HomePage />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/about" element={<About />} />
             <Route path="/cart" element={<Cart />} />
