a699498e5e8d1efae35fa7287211f5fe7a26bb7a
philo wael <reconstructed.philo@gmail.com>
2025-10-14 01:42:07 +0300

    Add to cart functionality

--- src/store/slices/cartSlice.jsx
+++ src/store/slices/cartSlice.jsx
@@ -11,14 +11,14 @@ const initialState = {
 const cartSlice = createSlice({
     name: "cart",
     initialState,
-    reducer: {
+    reducers: {
         addProduct(state, action){
-            const founded = state.cartBasket.find((p) => (p.id == action.payload.id))
+            const founded = state.cartBasket.find((p) => (p.id == action.payload.product.id))
             if (!founded){
-                state.cartBasket.push({...action.payload, quantity: 1})
+                state.cartBasket.push({...action.payload.product, quantity: action.payload.quantity})
                 state.totalProducts = state.cartBasket.length
             } else {
-                founded.quantity += 1
+                founded.quantity += action.payload.quantity
             }
         },
         removeProduct(state, action){
