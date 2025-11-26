be18482739c70c1b14b6b043724342256c3c52a1
AhmedSobhy2005 <112513094+Mohamedsobhymohamed@users.noreply.github.com>
2025-11-26 18:55:43 +0200

    styling

--- frontend/src/Types.ts
+++ frontend/src/Types.ts
@@ -1,21 +1,23 @@
 type TProduct = {
-    name: string,
-    description: string,
-    price: number,
-    img: string,
-    stock: number,
-    _id: string
+    name: string;
+    description: string;
+    price: number;
+    img: string;
+    stock: number;
+    _id: string;
+    colors?: string[];
+    sizes?: string[];
 }
 
-type TCartEntry = { productId: string, quantity: number }
+type TCartEntry = { productId: string; quantity: number }
 
 type TCart = TCartEntry[]
 
 type TUser = {
-    username: string,
-    email: string,
-    loyaltyPoints: number,
-    isAdminstartor: number
+    username: string;
+    email: string;
+    loyaltyPoints: number;
+    isAdmin: boolean;
 }
 
-export type { TProduct , TCartEntry , TCart , TUser}
+export type { TProduct, TCartEntry, TCart, TUser }
