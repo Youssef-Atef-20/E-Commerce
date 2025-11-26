84c69d45c6c2ddaa88caf809f1bd6658fd5718c7
philo wael <reconstructed.philo@gmail.com>
2025-11-26 17:01:35 +0200

    error

--- frontend/src/Api.ts
+++ frontend/src/Api.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
-import env from "./env";
-
-export const api = axios.create({
-    baseURL: env.BACKEND_URL + "/api",
-    withCredentials : true
-});
+// import env from "./env";
 
 // export const api = axios.create({
-//     baseURL: "/api",
+//     baseURL: env.BACKEND_URL + "/api",
 //     withCredentials : true
 // });
+
+export const api = axios.create({
+    baseURL: "/api",
+    withCredentials : true
+});
 
 export default api
