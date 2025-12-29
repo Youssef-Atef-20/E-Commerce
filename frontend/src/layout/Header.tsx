659729d18316e31910428b5abdf57c3074813176
Youssef Atef <hh3928850@gmail.com>
2025-12-29 02:54:46 +0200

    The Final Part of the DEPI Project

--- frontend/src/layout/Header.tsx
+++ frontend/src/layout/Header.tsx
@@ -99,7 +99,6 @@ const Header = () => {
                 onClick={() => navigate("/login")}
                 className="p-2 rounded-full hover:bg-gray-100 hover:text-green-500 transition"
               >
-                login
                 <LogIn className="w-6 h-6" />
               </button>
             )}
